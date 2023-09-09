5b1e213825955c1974b9b62f2d0251c7dbdfb23d
Ankan Das <127923458+ankan20@users.noreply.github.com>
2023-09-09 21:59:23 +0530

    add insert function

--- CircularLinkedList.c
+++ CircularLinkedList.c
@@ -1,141 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct Node
-{
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    struct Node *prev;
     int data;
     struct Node *next;
-} *Head;
-//Creating circular Linkedlist form array
-void Create(int A[], int n)
-{
-    int i;
-    struct Node *t, *last;
-    Head = (struct Node *)malloc(sizeof(struct Node));
-    Head->data = A[0];
-    Head->next = Head;
-    last = Head;
+}*first=NULL;
 
-    for (i = 1; i < n; i++)
-    {
-        t = (struct Node *)malloc(sizeof(struct Node));
-        t->data = A[i];
-        t->next = last->next;
-        last->next = t;
-        last = t;
+void creatNode(int A[],int n ){
+    struct Node *t,*last;
+    int i;
+    first=(struct Node *)malloc(sizeof(struct Node));
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=first;
+    for(i=1;i<n;i++){
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
     }
 }
+
 //Display function
-void Display(struct Node *h)
-{
-    do
-    {
-        printf("%d ", h->data);
-        h = h->next;
-    } while (h != Head);
+void Display(struct Node *p){
+    while(p){
+        printf("%d ",p->data);
+        p=p->next;
+    }
     printf("\n");
 }
-//Recursive display function
-void RDisplay(struct Node *h){
-    static int flag=0;
-    if(h!=Head || flag==0){
-        flag=1;
-        printf("%d ",h->data);
-        RDisplay(h->next);
-    }
-    flag=0;
-    
-}
 
-//Length of the LinkedList
-int Lenght(){
-    struct Node *p=Head;
-    int count=0;
-    do{
+//Lenght counting function
+int Length(struct Node *p){
+    int len=0;
+    while(p){
+        len++;
         p=p->next;
-        count++;
-    }while(p!=Head);
-    return count;
+    }
+    return len;
 }
 
-//Insert function for circular LinkedList
-void Insert(int index,int x){
-    struct Node *p=Head;
+//Insert function
+void Insert(struct Node *p,int index,int x){
     struct Node *t;
-    t=(struct Node *)malloc(sizeof(struct Node));
-    t->data=x;
-    if(index<0 || index>Lenght()){
+    int i;
+
+    if(index<0 || index>Length(p)){
         return ;
     }
-    if(index==0){
-        t->next=Head;
-        if(Head->next ==NULL){
-            Head->next=t;
-            Head=t;
-        }
-        else{
-            while(p->next!=Head){
-                p=p->next;
-            }
-            p->next=t;
-            Head=t;
-        }
+   if(index==0){
+    t=(struct Node *)malloc(sizeof(struct Node));
+    t->data=x;
+    t->next=first;
+    first->prev=t;
+    t->prev=NULL;
+    first=t;
+   }
+   else{
+    t=(struct Node *)malloc(sizeof(struct Node));
+    t->data=x;
+    for(i=0;i<index-1;i++){
+        p=p->next;
     }
-    else{
-        for(int i=0;i<index-1;i++){
-            p=p->next;
-        }
-        t->next=p->next;
-        p->next=t;
+    t->next=p->next;
+    t->prev=p;
+    if(p->next){
+        p->next->prev=t;
     }
+    p->next=t;
+   }
 }
 
-//Delete a node from LinkeList
-int Delete(int index,struct Node *p){
-    int x;
-    if(index<0 || index>Lenght()){
-        return -1;
-    }
-    if(index==1){
-        while(p->next!=Head){
-            p=p->next;
-        }
-        x=Head->data;
-        if(p==Head){
-            free(Head);
-            Head=NULL;
-        }
-        else{
-            p->next=Head->next;
-            
-            free(Head);
-            Head=p->next;
-        }
-        
-    }
-    else{
-        struct Node *q;
-        for(int i=0;i<index-2;i++){
-            p=p->next;
-        }
-        q=p->next;
-        p->next=q->next;
-        x=q->data;
-        free(q);
-        
-    }
-    return x;
-}
 
-int main()
-{
 
-    int A[] = {1, 2, 3, 4, 5};
-    Create(A, 5);
-    RDisplay(Head);
-    printf("\n");
-    Insert(1,11);
-    Display(Head);
-    int b=Delete(3,Head);
-    Display(Head);
+int main(){
+
+    int A[]={10,20,30,40,50};
+    creatNode(A,5);
+    printf("\nLength is :%d\n",Length(first));
+    Display(first);
+    Insert(first,0,100);
+    Display(first);
+
     return 0;
 }
