bb6cc6d9ce2e573ae810f0c2606b7759a9b040da
Ankan Das <127923458+ankan20@users.noreply.github.com>
2023-12-05 20:18:55 +0530

    Update BST.c

--- BST/BST.c
+++ BST/BST.c
@@ -81,15 +81,101 @@ void Inorder(Node *p)
     
 }
 
+//Recursive Insert function
+Node *RInsert(Node *p,int key)
+{
+    Node *t=NULL;
+    if(p==NULL)
+    {   
+        t=(Node *)malloc(sizeof(Node));
+        t->data=key;
+        t->rchild=t->lchild=NULL;
+        return t;
+    }
+    if(p->data > key)
+    {
+        p->lchild= RInsert(p->lchild,key);
+    }
+    else if(p->data < key)
+    {
+        p->rchild= RInsert(p->rchild,key);
+    }
+    
+    return p;
+    
+}
+//Height of BST
+int Height(Node *p)
+{   
+    int x,y;
+    if(p==NULL) return 0;
+    x=Height(p->lchild);
+    y=Height(p->rchild);
+    return x > y ? x+1 : y+1;
+}
+//Inorder Predecessor
+Node *InPre(Node *p)
+{
+    while(p && p->rchild!=NULL)
+        p=p->rchild;
+    return p;
+}
+//Inorder Successor
+Node *InSucc(Node *p)
+{
+    while(p && p->lchild!=NULL)
+        p=p->lchild;
+    return p;
+}
+//delete function
+Node *Delete(Node *p,int key)
+{   
+    Node *q;
+    if(p==NULL)
+    {
+        return NULL;
+    }
+    if(p->lchild==NULL && p->rchild==NULL)
+    {   
+        if(p==root)
+            root==NULL;
+        free(p);
+        return NULL;
+    }
+    if(key < p->data)
+    {
+        p->lchild=Delete(p->lchild,key);
+    }
+    else if(key > p->data)
+    {
+        p->rchild=Delete(p->rchild,key);
+    }
+    else{
+        if(Height(p->lchild)>Height(p->rchild))
+        {
+            q=InPre(p->lchild);
+            p->data=q->data;
+            p->lchild=Delete(p->lchild,q->data);
+        }
+        else{
+            q=InSucc(p->rchild);
+            p->data=q->data;
+            p->rchild=Delete(p->rchild,q->data);
+        }
+    }
+    return p;
+}
 int main()
 {
+    Insert(50);
     Insert(10);
-    Insert(5);
+    Insert(40);
     Insert(20);
-    Insert(8);
     Insert(30);
     Inorder(root);
     printf("NULL \n");
-    printf("%d ",Search(200));
+    
+    Delete(root,50);
+    Inorder(root);
     return 0;
 }
